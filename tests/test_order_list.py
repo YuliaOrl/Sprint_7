88f4de0dc54ed751f43460e06530018c2733b4f9
Orlova Yulia <yaorlova2@mts.ru>
2024-06-27 03:34:22 +0300

    Added test_order_list_without_courier_id

--- tests/test_order_list.py
+++ tests/test_order_list.py
@@ -15,12 +15,19 @@ class TestOrderList:
         login_response = requests.post(MAIN_URL + 'courier/login', data=payload)
         cls.courier_id = login_response.json()["id"]
 
-    @allure.title('Позитивная проверка получения списка заказов курьера по его ID')
-    @allure.description('Проверяется статус код и возвращение в теле ответа списка заказов')
-    @allure.step('Получение списка заказов курьера')
+    @allure.title('Позитивная проверка получения списка заказов нового курьера по его courierId')
+    @allure.description('Проверяется статус код и возвращение в теле ответа пустого списка заказов для нового курьера')
+    @allure.step('Получение списка заказов курьера по его courierId')
     def test_order_list_with_courier_id(self):
         response = requests.get(MAIN_URL + 'orders?courierId=' + str(self.courier_id))
         assert response.status_code == 200 and response.json()['orders'] == []
+
+    @allure.title('Позитивная проверка получения общего списка заказов без указания courierId')
+    @allure.description('Проверяется статус код и возвращение в теле ответа списка заказов')
+    @allure.step('Получение общего списка заказов')
+    def test_order_list_without_courier_id(self):
+        response = requests.get(MAIN_URL + 'orders')
+        assert response.status_code == 200 and not len(response.json()['orders']) == 0
 
     @classmethod
     def teardown_class(cls):
